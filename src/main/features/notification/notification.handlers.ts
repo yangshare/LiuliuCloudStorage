160c181c8622a26d2d55e786dcd275bc3fe14272
15708440182 <717449318@qq.com>
2026-05-17 17:02:38 +0800

    refactor: 移除 notification handler 中重复的 app:getVersion

--- src/main/features/notification/notification.handlers.ts
+++ src/main/features/notification/notification.handlers.ts
@@ -1,12 +1,8 @@
-import { ipcMain, app } from 'electron'
+import { ipcMain } from 'electron'
 import { notificationService } from '../../services/NotificationService'
 
 export function registerNotificationHandlers(): void {
   ipcMain.handle('notification:show', (_, options: { title: string; body: string }) => {
     notificationService.show(options.title, options.body, 'info')
-  })
-
-  ipcMain.handle('app:getVersion', () => {
-    return app.getVersion()
   })
 }
